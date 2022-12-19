c708ed7927eaa8c9968629db60bbfda3e72ccebe
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2022-12-19 20:58:49 -0300

    :recycle: refactor: change to final attibute

--- src/model/exceptions/ValidationException.java
+++ src/model/exceptions/ValidationException.java
@@ -7,7 +7,7 @@ public class ValidationException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
 
-	private Map<String, String> errors = new HashMap<>();
+	private final Map<String, String> errors = new HashMap<>();
 
 	public ValidationException(String msg) {
 		super(msg);
