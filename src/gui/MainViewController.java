7ae906f763b45765a926dc18c89680b746ab30f6
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2022-12-20 09:38:49 -0300

    :sparkles: feat: SellerList

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
+import model.services.SellerService;
 
 public class MainViewController implements Initializable {
 
@@ -31,7 +32,11 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemSellerAction() {
-		System.out.println("onMenuItemSellerAction");
+		Consumer<SellerListController> lambda = (SellerListController controller) -> {
+			controller.setSellerService(new SellerService());
+			controller.updateTableView();
+		};
+		loadView("/gui/SellerList.fxml", lambda);
 	}
 
 	@FXML
