d6d71c5e03015e5876c773fe9b12f5a796bff9a1
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2022-12-21 16:48:01 -0300

    :bug: fix: remove unnecessary import

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import java.util.ResourceBundle;
 
 import application.Main;
-import db.DbException;
 import db.DbIntegrityException;
 import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
